4edbff23198f47918f3bb83244c2947b38761997
rmrf <ryan.qian@gmail.com>
2025-03-26 15:27:42 +0800

    enable avante.vim

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight-storm]])
+			-- vim.cmd([[colorscheme tokyonight-storm]])
 			--vim.cmd([[colorscheme default]])
 		end,
 	},
@@ -237,6 +237,8 @@ require("lazy").setup({
 	{ "nvim-neotest/nvim-nio" },
 	{
 		"jellydn/hurl.nvim",
+        -- 如果没找到，那么使用 :HurlSetEnvFile 设置文件即可
+        env_file = { 'hurl.env', },
 		dependencies = {
 			"MunifTanjim/nui.nvim",
 			"nvim-lua/plenary.nvim",
@@ -251,6 +253,7 @@ require("lazy").setup({
 			-- Show response in popup or split
 			mode = "split",
 			-- Default formatter
+            -- find_env_files_in_folders = utils.find_env_files_in_folders,
 			formatters = {
 				json = { "jq" }, -- Make sure you have install jq in your system, e.g: brew install jq
 				html = {
@@ -414,6 +417,66 @@ require("lazy").setup({
             'stevearc/dressing.nvim', -- optional for vim.ui.select
         },
         config = true,
+    },
+    {
+      "yetone/avante.nvim",
+      event = "VeryLazy",
+      version = false, -- Never set this value to "*"! Never!
+      opts = {
+        -- add any opts here
+        -- for example
+        provider = "openai",
+        openai = {
+          endpoint = "https://api.deepseek.com/v1",
+          api_key_name = "DEEPSEEK_API_KEY",
+          model = "deepseek-chat", -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+          temperature = 0,
+          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+      },
+      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+      build = "make",
+      -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "echasnovski/mini.pick", -- for file_selector provider mini.pick
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+          -- support for image pasting
+          "HakonHarnes/img-clip.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- recommended settings
+            default = {
+              embed_image_as_base64 = false,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
+              -- required for Windows users
+              use_absolute_path = true,
+            },
+          },
+        },
+        {
+          -- Make sure to set this up properly if you have lazy=true
+          'MeanderingProgrammer/render-markdown.nvim',
+          opts = {
+            file_types = { "markdown", "Avante" },
+          },
+          ft = { "markdown", "Avante" },
+        },
+      },
     }
 
 })
